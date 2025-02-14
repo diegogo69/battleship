5f5b23a1acdf2d42976d8e017fa2a64873a67a09
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-02-13 22:28:56 -0400

    FEAT: Check if all ships are sunk
    On succesful attacks that result in hits
    Check if the hit ship have been sunk
    If so, check if all ships in the gameboard have been sunk
    If so return true
    If ship is hit but not all ships have been sunk return false
    If no ship was hit a attack was a miss return null

--- src/static/js/Gameboard.js
+++ src/static/js/Gameboard.js
@@ -1,24 +1,22 @@
 import Ship from "./Ship";
 
+// Ships fleet
+// 1 of 4
+// 2 of 3
+// 3 of 2
+// 4 of 1
+
 class Gameboard {
-  constructor() {
+  constructor(shipsNo = 5) {
+    this.shipsNo = shipsNo;
+    // sunks
+    this.sunks = 0;
+    // shipsarr32
+    this.ships = [];
     // Shots board
     this.shotsBoard = [...Array(10)].map(() => [...Array(10)].map(() => null));
     // Ships board
     this.shipsBoard = [...Array(10)].map(() => [...Array(10)].map(() => null));
-    // sunks
-    this.sunks = 0;
-    // shipsarr
-    this.ships = [];
-    // areSunk
-    
-    // receiveAttack
-
-    // Ships fleet
-    // 1 of 4
-    // 2 of 3
-    // 3 of 2
-    // 4 of 1
   }
 
   #boardSize = 10;
@@ -47,17 +45,24 @@ class Gameboard {
 
   // Return index of added ship
   // As it was added with push, it is the last item (e.i. length - 1)
-  #addShip(length) {
-    const arrLength = this.ships.push(new Ship(length));
-    return arrLength - 1;
+  #addShip(shipLength) {
+    const shipsArrLength = this.ships.push(new Ship(shipLength));
+    return shipsArrLength - 1;
+  }
+
+  areSunk() {
+    return this.sunks === this.shipsNo;
   }
 
   // placeShip(s)
   placeShip(rowcol, length, horizontal) {
+    if (this.shipsNo === this.ships.length)
+      throw "Cannot place more ships, limit has been reached";
+
     let { row, col } = this.#validateCoordinates(rowcol);
     if (!this.#canBePlaced(row, col, length, horizontal)) return false;
 
-    const shipIndex = this.#addShip();
+    const shipIndex = this.#addShip(length);
 
     for (let i = 0; i < length; i++) {
       this.shipsBoard[row][col] = shipIndex;
@@ -69,10 +74,30 @@ class Gameboard {
     return true;
   }
 
+  // Gameboards should keep track of missed attacks so they can display them properly
   receiveAttack(rowcol) {
     const { row, col } = this.#validateCoordinates(rowcol);
 
-    return this.shipsBoard[row][col] !== null;
+    // Check if attack is hit or miss. Register and return:
+    // Miss = null
+    // Hit = false
+    // Hit and all ships sunk = true
+    if (this.shipsBoard[row][col] !== null) {
+      const shipIndex = this.shipsBoard[row][col];
+      const ship = this.ships[shipIndex];
+      ship.hit();
+
+      if (ship.isSunk()) {
+        this.sunks += 1;
+        if (this.areSunk()) return true;
+      }
+
+      this.shotsBoard[row][col] = true;
+      return false;
+    }
+
+    this.shotsBoard[row][col] = false;
+    return null;
   }
 }
 
--- src/static/js/Gameboard.test.js
+++ src/static/js/Gameboard.test.js
@@ -190,32 +190,35 @@ describe("Reject when ship's length exceeds board bounds", () => {
 // or records the coordinates of the missed shot.
 
 describe("receiveAttack determines whether or not the attack hit a ship", () => {
+  // Miss = null
+  // Hit = false
+  // Hit and all ships sunk = true
   test("Valid attack hits ship on 00", () => {
     const gameboard = new Gameboard();
     gameboard.placeShip("00", 1, true);
 
-    expect(gameboard.receiveAttack("00")).toBe(true);
+    expect(gameboard.receiveAttack("00")).toBe(false);
   });
 
   test("Valid attack hits ship on 99", () => {
     const gameboard = new Gameboard();
     gameboard.placeShip("99", 1, true);
 
-    expect(gameboard.receiveAttack("99")).toBe(true);
+    expect(gameboard.receiveAttack("99")).toBe(false);
   });
   
   test("Invalid attack does not hits any ship at 00", () => {
     const gameboard = new Gameboard();
     gameboard.placeShip("01", 1, true);
 
-    expect(gameboard.receiveAttack("00")).toBe(false);
+    expect(gameboard.receiveAttack("00")).toBe(null);
   });
 
   test("Invalid attack does not hits any ship at 99", () => {
     const gameboard = new Gameboard();
     gameboard.placeShip("98", 1, true);
 
-    expect(gameboard.receiveAttack("99")).toBe(false);
+    expect(gameboard.receiveAttack("99")).toBe(null);
   });
 
   test("Reject invalid attack at 999", () => {
@@ -226,4 +229,64 @@ describe("receiveAttack determines whether or not the attack hit a ship", () => 
     }).toThrow("Invalid out of bounds coordinates");
   });
   
+});
+
+describe("Keep track of missed attacks", () => {
+  test("Trck missed attack at 00, as false in shotsBoard", () => {
+    const gameboard = new Gameboard();
+    gameboard.receiveAttack("00");
+
+    expect(gameboard.shotsBoard[0][0]).toBe(false);
+    expect(gameboard.shotsBoard[0][1]).toBe(null);
+    expect(gameboard.shotsBoard[1][0]).toBe(null);
+  });
+ 
+});
+
+// report whether or not all of their ships have been sunk
+describe("Report whether or not all of their ships have been sunk", () => {
+  test("Gameboard of three ships, place three single sqr ships and hit all of them", () => {
+    const shipsNo = 3;
+    const gameboard = new Gameboard(shipsNo);
+
+    gameboard.placeShip('00', 1, true);
+    gameboard.placeShip('10', 1, true);
+    gameboard.placeShip('20', 1, true);
+
+    expect(gameboard.receiveAttack("00")).toBe(false);
+    expect(gameboard.receiveAttack("10")).toBe(false);
+    expect(gameboard.receiveAttack("20")).toBe(true);
+  });
+
+  test("Gameboard of three ships, place three multi sqr ships and hit all of them", () => {
+    const shipsNo = 3;
+    const gameboard = new Gameboard(shipsNo);
+
+    gameboard.placeShip('00', 1, true);
+    gameboard.placeShip('10', 2, true);
+    gameboard.placeShip('20', 3, true);
+
+    expect(gameboard.receiveAttack("00")).toBe(false);
+    expect(gameboard.receiveAttack("10")).toBe(false);
+    expect(gameboard.receiveAttack("11")).toBe(false);
+    expect(gameboard.receiveAttack("20")).toBe(false);
+    expect(gameboard.receiveAttack("21")).toBe(false);
+    expect(gameboard.receiveAttack("22")).toBe(true);
+  });
+
+  test("Gameboard of three ships, place three multi sqr ships. Do not hit all of them", () => {
+    const shipsNo = 3;
+    const gameboard = new Gameboard(shipsNo);
+
+    gameboard.placeShip('00', 1, true);
+    gameboard.placeShip('10', 2, true);
+    gameboard.placeShip('20', 3, true);
+
+    expect(gameboard.receiveAttack("00")).toBe(false);
+    expect(gameboard.receiveAttack("10")).toBe(false);
+    expect(gameboard.receiveAttack("11")).toBe(false);
+    expect(gameboard.receiveAttack("20")).toBe(false);
+    expect(gameboard.receiveAttack("21")).toBe(false);
+    expect(gameboard.receiveAttack("23")).toBe(null);
+  });
 });
