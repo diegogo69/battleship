6d88ec44868eb098531dbab09ef4ae818835c4ec
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-02-18 11:12:52 -0400

    FEAT: Add pseudocode for a game factory

---
+++ src/static/js/gameInstance.js
@@ -0,0 +1,31 @@
+import domHandler from "./domHandler";
+import createBoard from "./createBoard";
+
+// Game instance module (Factory)
+const gameInstance = function gameInstance() {
+  // Create Player1
+  // Create Player2
+
+  // For testing place default ships for both players
+
+  // Create turn variable
+  // Change turn method
+  // getRivalTurn method
+
+  // Create a gameFinished promise
+  // It gets resolve at a winner
+
+  // Init the dom from the Players gameboards
+  // Add event handler for playing the turn
+
+  // Reference Player1 board
+  // Reference Player2 board
+  // Convert both reference (nodelist) into arrays
+
+  // Game is driven by event listeners until a winner
+
+  // At a winner
+  // Display boards with no event listeners
+  // Resolve promise
+  return {}
+}
