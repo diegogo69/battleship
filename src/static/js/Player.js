615e96f320e38c268d35875287c4585159bd9f17
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-02-18 22:16:35 -0400

    Add shipsNo parameter
    To initialize the player's gameboard to a defined
    Number of ships, that can be placed

--- src/static/js/Player.js
+++ src/static/js/Player.js
@@ -1,11 +1,11 @@
 import Gameboard from "./Gameboard";
 
 class Player {
-  constructor(realPlayer) {
+  constructor(realPlayer, shipsNo) {
     if (typeof realPlayer !== "boolean") throw Error("Invalid argument given. A boolean type is expected");
     
     this.type = realPlayer ? "real" : "computer";
-    this.gameboard = new Gameboard();
+    this.gameboard = new Gameboard(shipsNo);
   }
 }
 
