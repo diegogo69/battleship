47bbaee665b38def21be54c59f8dac779e68794a
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-02-18 22:24:33 -0400

    FEAT: Run basic game instance

--- src/index.js
+++ src/index.js
@@ -1,35 +1,26 @@
-import Ship from "./static/js/Ship";
-import Gameboard from "./static/js/Gameboard";
-import Player from "./static/js/Player";
+import "./static/css/modern-normalize.css";
+import "./static/css/styles.css";
+import "./static/css/gameboard.css";
+
+import gameInstance from "./static/js/gameInstance";
+
 
 const initGame = function initGame() {
-  // Create players
-  // One real player
-  const realPlayer = new Player(true);
-  // One computer player
-  const computerPlayer = new Player(false);
-  // Make random plays. Valid non duplicate coordinates.
 
-  // Populate each player's gameboard with predetermined coordinates
-  // Place three ships. Of len 1, 2, 2. For each
-  realPlayer.gameboard.placeShip("00", 1, true);
-  realPlayer.gameboard.placeShip("01", 2, true);
-  realPlayer.gameboard.placeShip("10", 2, false);
+  const game = gameInstance();
 
-  computerPlayer.gameboard.placeShip("00", 1, true);
-  computerPlayer.gameboard.placeShip("01", 2, true);
-  computerPlayer.gameboard.placeShip("10", 2, false);
+  const playTurn = function playTurn(e) {
+    game.handleTurn(e);
+    if (game.getWinner() !== null) {
+      alert(`Hello winner ${game.getWinner()}`);
+    }
+  }
+
+  game.init(playTurn);
+
+  // check winner if winner game = null
+  // Display new game
+
 };
 
-// Display / render both gameboards
-
-// Event listeners use only class methods
-
-// Allow attacks
-// Click on a board coordinate
-// Send input to appropiate methods
-// Update object state and re-render gameboard
-
-// Gamelogic
-// Handle game turns
-// End game when a player's ships have all been sunk
+initGame();
