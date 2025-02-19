c87abefe468c73681741a85d378fe214b4dac619
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-02-18 22:15:19 -0400

    FEAT: Add methods to control game flow
    Initialize game instance with predetermined ships
    Has attributes that tracks the two players, current turn, and
    Callback function used as event handler in board nodes
    This callback is given as an argument to the init method
    And a winner property
    init method create 2 player instances, and renders both boards
    displayBoard renders a board, update board does the same
    but it first empty the container node, removing previous state board
    handleTurn receives the click event, discarting it if
    It's not a valid coordinate, or it's on the player's own board
    Attack opponent board, and check if has won, invoking endGame
    endGame method sets the winner att to the current player turn
    And remove event listeners from both boards, so the game stops

--- src/static/js/gameInstance.js
+++ src/static/js/gameInstance.js
@@ -1,31 +1,101 @@
 import domHandler from "./domHandler";
 import createBoard from "./createBoard";
+import Player from "./Player";
+
+const testPlaceShips = function (player) {
+  player.gameboard.placeShip("00", 1, true);
+  player.gameboard.placeShip("01", 2, true);
+  player.gameboard.placeShip("10", 2, false);
+};
 
 // Game instance module (Factory)
 const gameInstance = function gameInstance() {
   // Create Player1
   // Create Player2
+  const players = {
+    player1: null,
+    player2: null,
+  };
 
+  let callback = null;
+
+  const init = function init(fn) {
+    callback = fn;
+
+    players.player1 = new Player(true, 3);
+    players.player2 = new Player(false, 3);
+    // Populate each player's gameboard with predetermined coordinates
+    // Place three ships. Of len 1, 2, 2. For each
+    testPlaceShips(players.player1);
+    testPlaceShips(players.player2);
+
+    displayBoard(1);
+    displayBoard(2);
+  };
+  let winner = null;
+  const getWinner = function getWinner() {
+    return winner
+  }
   // For testing place default ships for both players
 
-  // Create turn variable
-  // Change turn method
-  // getRivalTurn method
+  let turn = 1;
 
-  // Create a gameFinished promise
-  // It gets resolve at a winner
+  const getRivalTurn = function () {
+    return turn === 1 ? 2 : 1;
+  };
 
-  // Init the dom from the Players gameboards
-  // Add event handler for playing the turn
+  const changeTurn = function () {
+    turn = getRivalTurn();
+  };
 
-  // Reference Player1 board
-  // Reference Player2 board
-  // Convert both reference (nodelist) into arrays
+  const displayBoard = function displayBoard(playerNo) {
+    const player = `player${playerNo}`;
+    const board = createBoard(players[player].gameboard, playerNo, callback);
+    domHandler.render.board[player](board);
+  };
+  const updateBoard = function updateBoard(playerNo) {
+    const player = `player${playerNo}`;
 
-  // Game is driven by event listeners until a winner
+    domHandler.clear[player]();
+    const board = createBoard(players[player].gameboard, playerNo, callback);
+    domHandler.render.board[player](board);
+  };
 
-  // At a winner
-  // Display boards with no event listeners
-  // Resolve promise
-  return {}
-}
+  const playTurn = function playTurn(rival, rowcol) {
+    return players[`player${rival}`].gameboard.receiveAttack(rowcol);
+  };
+
+  const handleTurn = function handleTurn(e) {
+    const rowcol = e.target.dataset.rowcol;
+    if (!rowcol) return;
+
+    if (e.currentTarget.dataset.boardNo == turn) {
+      console.log("Ignored. Player click on its own board");
+      return;
+    }
+
+    const rival = getRivalTurn();
+
+    const gameover = playTurn(rival, rowcol);
+    updateBoard(rival);
+
+    if (gameover === true) endGame();
+    else changeTurn();
+  };
+
+  const endGame = function endGame() {
+    winner = turn;
+
+    domHandler.removeEventListener.player1('click', callback)
+    domHandler.removeEventListener.player2('click', callback)
+    // Determine winner based on current turn
+
+    // Remove board event listeners
+    // Show winner msg
+    // Show new game button
+  };
+
+  return { init, getWinner, handleTurn };
+};
+
+export default gameInstance;
