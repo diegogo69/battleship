91a97fb37857b89d610024d8ce4291acb8825349
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-04-08 15:09:20 -0400

    FEAT: Add initHandlers and refac method names
    Add initHandlers method to add event handlers to board containers on click
    Also refac name for utility methods for simplicity
    Making use of objects valid numeric property names

--- src/static/js/domHandler.js
+++ src/static/js/domHandler.js
@@ -2,14 +2,19 @@ const domHandler = (function () {
   const player1Container = document.querySelector(".player1-container");
   const player2Container = document.querySelector(".player2-container");
 
+  const initHandlers = function(e, fn) {
+    player1Container.addEventListener(e, fn)
+    player2Container.addEventListener(e, fn)
+  }
+
   const removeEventListener = {
     node(node, type, fn) {
       node.removeEventListener(type, fn)
     },
-    player1(type, fn) {
+    1: function(type, fn) {
       this.node(player1Container.firstChild, type, fn)
     },
-    player2(type, fn) {
+    2: function(type, fn) {
       this.node(player2Container.firstChild, type, fn)
     },
   }
@@ -19,28 +24,32 @@ const domHandler = (function () {
         node.removeChild(node.firstChild);
       }
     },
-    player1() {
-      this.node(player1Container);
-    },
-
-    player2() {
-      this.node(player2Container);
-    },
+    player: {
+      1: function() {
+        clear.node(player1Container);
+      },
+  
+      2: function() {
+        clear.node(player2Container);
+      },
+    }
   };
 
   const render = {
     board: {
-      player1(boardNode) {
+      1: function(boardNode) {
+        clear.player[1]()
         player1Container.appendChild(boardNode);
       },
 
-      player2(boardNode) {
+      2: function(boardNode) {
+        clear.player[2]()
         player2Container.appendChild(boardNode);
       },
     },
   };
 
-  return { clear, render, removeEventListener };
+  return { clear, render, removeEventListener, initHandlers };
 })();
 
 export default domHandler;
