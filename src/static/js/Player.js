bb159948971eee11b37139985dce7bf345c19af5
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-04-08 15:02:13 -0400

    FEAT: Add methods for AI random plays
    In case player is of type computer create an array of availabe valid moves
    And a method for returning coordinates for a valid move

--- src/static/js/Player.js
+++ src/static/js/Player.js
@@ -6,7 +6,36 @@ class Player {
     
     this.type = realPlayer ? "real" : "computer";
     this.gameboard = new Gameboard(shipsNo);
+    
+    this.availableMoves = [];  
+    if (!realPlayer) {
+      // Populate all possible moves
+      for (let row = 0; row < this.gameboard.boardSize; row++) {
+        for (let col = 0; col < this.gameboard.boardSize; col++) {
+            // this.availableMoves.push([row, col]);
+            this.availableMoves.push(`${row}${col}`);
+
+        }
+      }
+    }
   }
+
+
+  generateRandomMove() {
+    if (this.availableMoves.length === 0) {
+        throw new Error("No more available moves");
+    }
+    
+    // Choose a random rowcol
+    const randomIndex = Math.floor(Math.random() * this.availableMoves.length);
+    const rowcol = this.availableMoves[randomIndex];
+
+    // Remove the chosen rowcol from the available moves
+    this.availableMoves.splice(randomIndex, 1);
+
+    return rowcol;
+  }
+
 }
 
 export default Player;
