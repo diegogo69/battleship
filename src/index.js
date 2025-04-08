e4048d0fba7a28fcfafd551bbb12fdde5d6ec33c
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-04-08 15:15:34 -0400

    REFAC: Init default game instance

--- src/index.js
+++ src/index.js
@@ -4,23 +4,12 @@ import "./static/css/gameboard.css";
 
 import gameInstance from "./static/js/gameInstance";
 
-
 const initGame = function initGame() {
-
   const game = gameInstance();
-
-  const playTurn = function playTurn(e) {
-    game.handleTurn(e);
-    if (game.getWinner() !== null) {
-      alert(`Hello winner ${game.getWinner()}`);
-    }
-  }
-
-  game.init(playTurn);
-
-  // check winner if winner game = null
-  // Display new game
-
+  game.init();
 };
+
+const newGameBtn = document.querySelector('.new-game-btn')
+newGameBtn.addEventListener('click', initGame)
 
 initGame();
