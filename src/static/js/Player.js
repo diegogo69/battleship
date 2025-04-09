9d66e557394f8416f2d606d098facc0716e9e5a3
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-04-08 20:51:41 -0400

    FEAT: Add AIPlayer class extends Player
    AIPlayer incorpores a property and method
    To get random valid coordinates for moves

--- src/static/js/Player.js
+++ src/static/js/Player.js
@@ -1,25 +1,27 @@
 import Gameboard from "./Gameboard";
 
 class Player {
-  constructor(realPlayer, shipsNo) {
-    if (typeof realPlayer !== "boolean") throw Error("Invalid argument given. A boolean type is expected");
-    
-    this.type = realPlayer ? "real" : "computer";
+  constructor(shipsNo) {
+    this.type = "real";
     this.gameboard = new Gameboard(shipsNo);
-    
-    this.availableMoves = [];  
-    if (!realPlayer) {
-      // Populate all possible moves
-      for (let row = 0; row < this.gameboard.boardSize; row++) {
-        for (let col = 0; col < this.gameboard.boardSize; col++) {
-            // this.availableMoves.push([row, col]);
-            this.availableMoves.push(`${row}${col}`);
+  }
+}
 
-        }
+class AIPlayer extends Player {
+  constructor(shipsNo) {
+    super(shipsNo);
+    this.type = "computer";
+    this.availableMoves = [];  
+
+    // Populate all possible moves
+    for (let row = 0; row < this.gameboard.boardSize; row++) {
+      for (let col = 0; col < this.gameboard.boardSize; col++) {
+          // this.availableMoves.push([row, col]);
+          this.availableMoves.push(`${row}${col}`);
+
       }
     }
   }
-
 
   generateRandomMove() {
     if (this.availableMoves.length === 0) {
@@ -29,13 +31,12 @@ class Player {
     // Choose a random rowcol
     const randomIndex = Math.floor(Math.random() * this.availableMoves.length);
     const rowcol = this.availableMoves[randomIndex];
-
+  
     // Remove the chosen rowcol from the available moves
     this.availableMoves.splice(randomIndex, 1);
-
+  
     return rowcol;
   }
-
 }
 
-export default Player;
+export { Player, AIPlayer};
