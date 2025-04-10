7a6ce1a82ea8dd05fff7bd8746c8150f56faee42
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-04-10 14:16:39 -0400

    REFAC: decouple non fundamental functionality

--- src/static/js/gameInstance.js
+++ src/static/js/gameInstance.js
@@ -1,48 +1,34 @@
 import domHandler from "./domHandler";
-import createBoard from "./createBoard";
+import { displayBoard, testPlaceShips } from "./eventHandlers";
 import { Player, AIPlayer } from "./Player";
-
-const testPlaceShips = function (player) {
-  player.gameboard.placeShip("00", 1, true);
-  player.gameboard.placeShip("01", 2, true);
-  player.gameboard.placeShip("10", 2, false);
-};
 
 const gameInstance = function gameInstance() {
   // Create players. One real, one computer. Populate each gameboard
   const players = {
     1: null,
     2: null,
-  }
+  };
   let turn = 1;
   let winner = null;
 
-
-  // Display gameboards
-  const displayBoard = function displayBoard(playerNo) {
-    const player = players[playerNo];
-    const board = createBoard(player.gameboard, playerNo);
-    domHandler.render.board[playerNo](board);
-  };
-
   const init = function init(pvp) {
     players[1] = new Player(3);
-    if (pvp === true) players[2] = new Player(3) 
-    else players[2] = new AIPlayer(3)
+    if (pvp === true) players[2] = new Player(3);
+    else players[2] = new AIPlayer(3);
 
-    testPlaceShips(players[1]);
-    testPlaceShips(players[2]);
+    // testPlaceShips(players[1]);
+    // testPlaceShips(players[2]);
 
-    displayBoard(1);
-    displayBoard(2);
+    const player = displayBoard(1, players[1].gameboard);
+    displayBoard(2, players[2].gameboard);
 
-    domHandler.initHandlers('click', handleTurn)
-  }
+    domHandler.initHandlers("click", handleTurn);
+  };
 
   const endGame = function endGame() {
-    winner = turn
-    alert('winner is player' + turn)
-  }
+    winner = turn;
+    alert("winner is player" + turn);
+  };
 
   const getRivalTurn = function () {
     return turn === 1 ? 2 : 1;
@@ -57,7 +43,7 @@ const gameInstance = function gameInstance() {
   };
 
   const handleTurn = function handleTurn(e) {
-    if (winner) return
+    if (winner) return;
 
     const rowcol = e.target.dataset.rowcol;
     if (!rowcol) return;
@@ -70,27 +56,23 @@ const gameInstance = function gameInstance() {
     const rival = getRivalTurn();
     let attack = playTurn(rival, rowcol);
     // If attacking the same spot twice
-    if (attack === null) return
+    if (attack === null) return;
     displayBoard(rival);
 
-    let gameover = players[rival].gameboard.areSunk()
+    let gameover = players[rival].gameboard.areSunk();
     if (gameover === true) endGame();
-
-    else if (players[rival].type === 'computer') {
+    else if (players[rival].type === "computer") {
       const AIrowcol = players[rival].generateRandomMove();
       const AIrival = turn;
-      attack = playTurn(AIrival, AIrowcol)
+      attack = playTurn(AIrival, AIrowcol);
       displayBoard(AIrival);
 
-      let gameover = players[AIrival].gameboard.areSunk()
+      let gameover = players[AIrival].gameboard.areSunk();
       if (gameover === true) endGame();
-    }
-
-    else changeTurn();
+    } else changeTurn();
   };
 
-  return { init, }
-}
-
+  return { init };
+};
 
 export default gameInstance;
