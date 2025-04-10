c399287f4888bb5b1173fe730e990e1e67db0a56
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-04-10 14:13:22 -0400

    FEAT: Add drop handlers to ganeboard cells
    createBoard function expects handler for drop, dragover and draggleave events
    as arguments, each set with a default importet from the eventHandler module
    This way each gameboard cell handles pertinent events for allocating a dragged ship

--- src/static/js/createBoard.js
+++ src/static/js/createBoard.js
@@ -1,24 +1,35 @@
+import {
+  dropHandler,
+  dragoverHandler,
+  dragleaveHandler,
+} from "./eventHandlers";
 // Function to create a board node, of the same structure as a board array
 // A board node, with row nodes, each with col nodes
 // board argument is expected to be a Gameboard object wich contains
 // a shipsBoard array with the board ships
 // a shotsBoard array with the received attacks
 // The board dipslays a player ships, and the received shots
-const createBoard = function createBoard(gameboard, boardNo, clickCallback) {
-  const boardNode = document.createElement('div');
-  boardNode.classList.add('gameboard');
+const createBoard = function createBoard(
+  gameboard,
+  boardNo,
+  dropFn = dropHandler,
+  dragoverFn = dragoverHandler,
+  dragleaveFn = dragleaveHandler,
+) {
+  const boardNode = document.createElement("div");
+  boardNode.classList.add("gameboard");
   boardNode.dataset.boardNo = boardNo;
-  
+
   const shipsBoard = gameboard.shipsBoard;
   const shotsBoard = gameboard.shotsBoard;
-  
+
   shipsBoard.forEach(function iterateRows(row, rowIndex) {
-    const rowNode = document.createElement('div');
-    rowNode.classList.add('gameboard-row');
+    const rowNode = document.createElement("div");
+    rowNode.classList.add("gameboard-row");
 
     shipsBoard[rowIndex].forEach(function iterateCols(col, colIndex) {
-      const colNode = document.createElement('div');
-      colNode.classList.add('gameboard-col');
+      const colNode = document.createElement("div");
+      colNode.classList.add("gameboard-col");
       colNode.dataset.rowcol = `${rowIndex}${colIndex}`;
 
       // Each node may contain classes that identify if:
@@ -26,29 +37,32 @@ const createBoard = function createBoard(gameboard, boardNo, clickCallback) {
       const shipIndex = shipsBoard[rowIndex][colIndex];
       if (shipIndex !== null) {
         colNode.dataset.shipIndex = shipIndex;
-        colNode.classList.add('hasShip');
+        colNode.classList.add("hasShip");
         colNode.textContent = shipIndex;
-
       }
 
-      if (shotsBoard [rowIndex][colIndex] === false) {
-        colNode.classList.add('isMiss');
-        // colNode.textContent = 'o';
+      if (shotsBoard[rowIndex][colIndex] === false) {
+        colNode.classList.add("isMiss");
       }
 
-      if (shotsBoard [rowIndex][colIndex] === true) {
-        colNode.classList.add('isHit');
-        // colNode.textContent = 'x';
+      if (shotsBoard[rowIndex][colIndex] === true) {
+        colNode.classList.add("isHit");
       }
 
+      if (dropFn)
+        colNode.addEventListener("drop", (e) => {
+          dropFn(e, gameboard);
+        });
+      if (dragoverFn) colNode.addEventListener("dragover", dragoverFn);
+      if (dragleaveFn) colNode.addEventListener("dragleave", dragleaveFn);
       rowNode.appendChild(colNode);
-    })
+    });
 
     boardNode.appendChild(rowNode);
-  })
+  });
 
-  boardNode.addEventListener('click', clickCallback);
+  // boardNode.addEventListener('click', clickFn);
   return boardNode;
-}
+};
 
 export default createBoard;
