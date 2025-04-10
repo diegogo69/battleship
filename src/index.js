c2a7ef9787c74627d501ba1e90b27cd65df13a01
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-04-10 14:00:54 -0400

    REFAC: simplify to dom initialization only

--- src/index.js
+++ src/index.js
@@ -2,14 +2,6 @@ import "./static/css/modern-normalize.css";
 import "./static/css/styles.css";
 import "./static/css/gameboard.css";
 
-import gameInstance from "./static/js/gameInstance";
+import initDom from "./static/js/domInit";
 
-const initGame = function initGame(pvp) {
-  const game = gameInstance();
-  game.init(pvp);
-};
-
-const newPvPBtn = document.querySelector('.new-pvp-btn')
-const newPvCBtn = document.querySelector('.new-pvc-btn')
-newPvPBtn.addEventListener('click', () => initGame(true))
-newPvCBtn.addEventListener('click', () => initGame(false))
+initDom()
