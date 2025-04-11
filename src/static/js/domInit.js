5f05364944058ca74dd4cbee85daec5c864a65e0
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-04-11 13:46:31 -0400

    FEAT: Remove test draggable ships

--- src/static/js/domInit.js
+++ src/static/js/domInit.js
@@ -1,19 +1,10 @@
-import { initGame, dragstartHandler } from "./eventHandlers";
+import { initGame } from "./handlers";
 
 const initDom = function () {
   const newPvPBtn = document.querySelector(".new-pvp-btn");
   const newPvCBtn = document.querySelector(".new-pvc-btn");
   newPvPBtn.addEventListener("click", () => initGame(true));
   newPvCBtn.addEventListener("click", () => initGame(false));
-
-  const ships = document.querySelectorAll(".ship");
-  ships.forEach((ship) => {
-    ship.addEventListener("dragstart", dragstartHandler);
-
-    ship.addEventListener("dragend", () => {
-      ship.classList.remove("dragging");
-    });
-  });
 };
 
 export default initDom
