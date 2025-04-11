1d643a0a4b6462b2de46eaeb31b0ad01ac48beba
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-04-11 13:43:44 -0400

    FEAT: Make it a IIFE
    Refac module into a IIFE to enable the option of toggling functionality
    by changing the module state, setting the values for drag and drop handlers
    to null or setting them to their respective handlers
    Enabling them for when ships are going to be positioned
    Disabling drag and drop  when are ships are already placed

--- src/static/js/createBoard.js
+++ src/static/js/createBoard.js
@@ -1,68 +1,86 @@
-import {
-  dropHandler,
-  dragoverHandler,
-  dragleaveHandler,
-} from "./eventHandlers";
+import { dropHandler, dragoverHandler, dragleaveHandler } from "./handlers";
 // Function to create a board node, of the same structure as a board array
 // A board node, with row nodes, each with col nodes
 // board argument is expected to be a Gameboard object wich contains
 // a shipsBoard array with the board ships
 // a shotsBoard array with the received attacks
 // The board dipslays a player ships, and the received shots
-const createBoard = function createBoard(
-  gameboard,
-  boardNo,
-  dropFn = dropHandler,
-  dragoverFn = dragoverHandler,
-  dragleaveFn = dragleaveHandler,
-) {
-  const boardNode = document.createElement("div");
-  boardNode.classList.add("gameboard");
-  boardNode.dataset.boardNo = boardNo;
 
-  const shipsBoard = gameboard.shipsBoard;
-  const shotsBoard = gameboard.shotsBoard;
+const createBoard = (function () {
+  let dropFn = null;
+  let dragoverFn = null;
+  let dragleaveFn = null;
 
-  shipsBoard.forEach(function iterateRows(row, rowIndex) {
-    const rowNode = document.createElement("div");
-    rowNode.classList.add("gameboard-row");
+  const enableDragDrop = function enableDragDrop() {
+    dropFn = dropHandler;
+    dragoverFn = dragoverHandler;
+    dragleaveFn = dragleaveHandler;
+  };
 
-    shipsBoard[rowIndex].forEach(function iterateCols(col, colIndex) {
-      const colNode = document.createElement("div");
-      colNode.classList.add("gameboard-col");
-      colNode.dataset.rowcol = `${rowIndex}${colIndex}`;
+  const disableDragDrop = function disableDragDrop() {
+    dropFn = null;
+    dragoverFn = null;
+    dragleaveFn = null;
+  };
 
-      // Each node may contain classes that identify if:
-      // contains a ship, have been shot (miss or hit)
-      const shipIndex = shipsBoard[rowIndex][colIndex];
-      if (shipIndex !== null) {
-        colNode.dataset.shipIndex = shipIndex;
-        colNode.classList.add("hasShip");
-        colNode.textContent = shipIndex;
-      }
+  const boardNode = function boardNode(gameboard, boardNo) {
+    const boardNode = document.createElement("div");
+    boardNode.classList.add("gameboard");
+    boardNode.dataset.boardNo = boardNo;
 
-      if (shotsBoard[rowIndex][colIndex] === false) {
-        colNode.classList.add("isMiss");
-      }
+    const shipsBoard = gameboard.shipsBoard;
+    const shotsBoard = gameboard.shotsBoard;
 
-      if (shotsBoard[rowIndex][colIndex] === true) {
-        colNode.classList.add("isHit");
-      }
+    shipsBoard.forEach(function iterateRows(row, rowIndex) {
+      const rowNode = document.createElement("div");
+      rowNode.classList.add("gameboard-row");
 
-      if (dropFn)
-        colNode.addEventListener("drop", (e) => {
-          dropFn(e, gameboard);
-        });
-      if (dragoverFn) colNode.addEventListener("dragover", dragoverFn);
-      if (dragleaveFn) colNode.addEventListener("dragleave", dragleaveFn);
-      rowNode.appendChild(colNode);
+      shipsBoard[rowIndex].forEach(function iterateCols(col, colIndex) {
+        const colNode = document.createElement("div");
+        colNode.classList.add("gameboard-col");
+        colNode.dataset.rowcol = `${rowIndex}${colIndex}`;
+
+        // Each node may contain classes that identify if:
+        // contains a ship, have been shot (miss or hit)
+        const shipIndex = shipsBoard[rowIndex][colIndex];
+        if (shipIndex !== null) {
+          colNode.dataset.shipIndex = shipIndex;
+          colNode.classList.add("hasShip");
+          colNode.textContent = shipIndex;
+        }
+
+        if (shotsBoard[rowIndex][colIndex] === false) {
+          colNode.classList.add("isMiss");
+        }
+
+        if (shotsBoard[rowIndex][colIndex] === true) {
+          colNode.classList.add("isHit");
+        }
+
+        // Add drag and drop handlers if enabled
+        if (dropFn) {
+          colNode.addEventListener("drop", (e) => {
+            dropFn(e, gameboard);
+          });
+        }
+        if (dragoverFn) colNode.addEventListener("dragover", dragoverFn);
+        if (dragleaveFn) colNode.addEventListener("dragleave", dragleaveFn);
+
+        rowNode.appendChild(colNode);
+      });
+
+      boardNode.appendChild(rowNode);
     });
 
-    boardNode.appendChild(rowNode);
-  });
+    // boardNode.addEventListener('click', clickFn);
+    return boardNode;
+  };
 
-  // boardNode.addEventListener('click', clickFn);
-  return boardNode;
-};
+  return {
+    boardNode,
+    enableDragDrop,
+    disableDragDrop,
+  };
+})();
 
 export default createBoard;
