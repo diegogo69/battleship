999856830c3a9c05a6bb40f40191fa794fc1a993
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-04-15 14:13:18 -0400

    REFAC: callbacks as default arguments

--- src/static/js/createShips.js
+++ src/static/js/createShips.js
@@ -1,31 +1,35 @@
 import handlers from "./handlers";
 
 const SHIPS_NO = 3;
-const createShips = function () {
-    const shipContainer = document.createElement('div');
-    shipContainer.classList.add('ships-container')
+const createShips = function (
+  dragstartFn = handlers.dragstart,
+  dragendFn = handlers.dragend,
+) {
+  const shipContainer = document.createElement("div");
+  shipContainer.classList.add("ships-container");
 
-    for (let i = 0; i < SHIPS_NO; i++) {
-        const shipLength = i + 1;
-        
-        const ship = document.createElement('div');
-        ship.setAttribute('id', shipLength)
-        ship.classList.add('ship');
-        ship.setAttribute('draggable', true)
-        ship.dataset.length = shipLength;
-        ship.dataset.orientation = 'horizontal';
+  for (let i = 0; i < SHIPS_NO; i++) {
+    const shipLength = i + 1;
 
-        for (let j = 0; j < shipLength; j++) {
-            const shipCell = document.createElement('div')
-            shipCell.classList.add('ship-cell')
-            ship.appendChild(shipCell)
-        }
+    const ship = document.createElement("div");
+    ship.setAttribute("id", shipLength);
+    ship.classList.add("ship");
+    ship.setAttribute("draggable", true);
+    ship.dataset.length = shipLength;
+    ship.dataset.orientation = "horizontal";
 
-        ship.addEventListener('dragstart', handlers.dragstart)
-        shipContainer.appendChild(ship)
+    for (let j = 0; j < shipLength; j++) {
+      const shipCell = document.createElement("div");
+      shipCell.classList.add("ship-cell");
+      ship.appendChild(shipCell);
     }
 
-    return shipContainer;
-}
+    ship.addEventListener("dragstart", dragstartFn);
+    ship.addEventListener("dragend", dragendFn);
+    shipContainer.appendChild(ship);
+  }
 
-export default createShips
+  return shipContainer;
+};
+
+export default createShips;
