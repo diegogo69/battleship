e51f5dbbbec55088217858a217841727beea91ed
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-04-19 10:13:47 -0400

    REFAC: Modularize main nodes

--- src/static/js/createMainPage.js
+++ src/static/js/createMainPage.js
@@ -1,53 +1,81 @@
-const createMainPage = function () {
-  const gameWrapper = document.createElement("div");
-  gameWrapper.classList.add("game-container");
+const createMainPage = ( function () {
+  const header = function header() {
+    const boardsHeader = document.createElement("header");
+    boardsHeader.classList.add("boards-header");
+    const headerText = document.createElement("h2");
+    headerText.textContent = "Are you ready for the battle?";
+    boardsHeader.appendChild(headerText)
 
-  const boardsHeader = document.createElement("header");
-  boardsHeader.classList.add("boards-header");
-  const headerText = document.createElement("h2");
-  headerText.textContent = "Are you ready for the battle?";
+    return boardsHeader;
+  };
 
-  boardsHeader.appendChild(headerText);
+  const boards = function boards() {
+    const boardsContainer = document.createElement("div");
+    boardsContainer.classList.add("boards-container");
 
-  const boardsContainer = document.createElement("div");
-  boardsContainer.classList.add("boards-container");
+    const p1Container = document.createElement("div");
+    p1Container.classList.add("player1-container");
 
-  const p1Container = document.createElement("div");
-  p1Container.classList.add("player1-container");
+    const p2Container = document.createElement("div");
+    p2Container.classList.add("player2-container");
 
-  const p2Container = document.createElement("div");
-  p2Container.classList.add("player2-container");
+    boardsContainer.appendChild(p1Container);
+    boardsContainer.appendChild(p2Container);
 
-  boardsContainer.appendChild(p1Container);
-  boardsContainer.appendChild(p2Container);
+    return boardsContainer;
+  };
 
-  const boardsFooter = document.createElement("footer");
-  boardsFooter.classList.add("boardsFooter");
-  const footerText = document.createElement("p");
-  footerText.textContent = "Select a game mode:";
+  const footer = function footer(gamemodes=true) {
+    const boardsFooter = document.createElement("footer");
+    boardsFooter.classList.add("boardsFooter");
+    const footerText = document.createElement("p");
+    footerText.classList.add('footerText');
+    const btnWrapper = document.createElement("div");
+    btnWrapper.classList.add("boards-btns");
 
-  const btnWrapper = document.createElement("div");
-  btnWrapper.classList.add("boards-btns");
+    if (gamemodes === true) {
+      footerText.textContent = "Select a game mode:";
 
-  const newPvCBtn = document.createElement("button");
-  newPvCBtn.classList.add("new-pvc-btn");
-  newPvCBtn.textContent = "1 player";
+      const newPvCBtn = document.createElement("button");
+      newPvCBtn.classList.add("new-pvc-btn");
+      newPvCBtn.textContent = "1 player";
 
-  const newPvPBtn = document.createElement("button");
-  newPvPBtn.classList.add("new-pvp-btn");
-  newPvPBtn.textContent = "2 player";
+      const newPvPBtn = document.createElement("button");
+      newPvPBtn.classList.add("new-pvp-btn");
+      newPvPBtn.textContent = "2 player";
 
-  btnWrapper.appendChild(newPvCBtn);
-  btnWrapper.appendChild(newPvPBtn);
+      btnWrapper.appendChild(newPvCBtn);
+      btnWrapper.appendChild(newPvPBtn);
+    } else {
+      const goBackBtn = document.createElement("button");
+      goBackBtn.classList.add("go-back-btn");
+      goBackBtn.textContent = "Back to menu";
 
-  boardsFooter.appendChild(footerText);
-  boardsFooter.appendChild(btnWrapper);
+      btnWrapper.appendChild(goBackBtn);
+    }
 
-  gameWrapper.appendChild(boardsHeader);
-  gameWrapper.appendChild(boardsContainer);
-  gameWrapper.appendChild(boardsFooter);
+    boardsFooter.appendChild(footerText);
+    boardsFooter.appendChild(btnWrapper);
 
-  return gameWrapper;
-};
+    return boardsFooter;
+  };
+
+  const main = function main() {
+    const gameWrapper = document.createElement("div");
+    gameWrapper.classList.add("game-container");
+  
+    const boardsHeader = header();
+    const boardsContainer = boards();
+    const boardsFooter = footer();
+
+    gameWrapper.appendChild(boardsHeader);
+    gameWrapper.appendChild(boardsContainer);
+    gameWrapper.appendChild(boardsFooter);
+    
+    return gameWrapper;
+  }
+
+  return { main, footer }
+}) ();
 
 export default createMainPage;
