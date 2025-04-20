fb3ab2ef379fc1e3abed81945cf341d4e6a2d17f
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-04-19 22:49:02 -0400

    REFAC: use helper method from Gameboard class

--- src/static/js/Player.js
+++ src/static/js/Player.js
@@ -11,32 +11,25 @@ class AIPlayer extends Player {
   constructor(shipsNo) {
     super(shipsNo);
     this.type = "computer";
-    this.availableMoves = [];  
-
-    // Populate all possible moves
-    for (let row = 0; row < this.gameboard.boardSize; row++) {
-      for (let col = 0; col < this.gameboard.boardSize; col++) {
-          // this.availableMoves.push([row, col]);
-          this.availableMoves.push(`${row}${col}`);
-
-      }
-    }
+    this.availableCoordinates = Gameboard.getValidCoordinates();
   }
 
   generateRandomMove() {
-    if (this.availableMoves.length === 0) {
-        throw new Error("No more available moves");
+    if (this.availableCoordinates.length === 0) {
+      throw new Error("No more available moves");
     }
-    
+
     // Choose a random rowcol
-    const randomIndex = Math.floor(Math.random() * this.availableMoves.length);
-    const rowcol = this.availableMoves[randomIndex];
-  
+    const randomIndex = Math.floor(
+      Math.random() * this.availableCoordinates.length,
+    );
+    const rowcol = this.availableCoordinates[randomIndex];
+
     // Remove the chosen rowcol from the available moves
-    this.availableMoves.splice(randomIndex, 1);
-  
+    this.availableCoordinates.splice(randomIndex, 1);
+
     return rowcol;
   }
 }
 
-export { Player, AIPlayer};
+export { Player, AIPlayer };
